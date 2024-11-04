ae6424b73a9110a4088d94d3edb5b17c4cb7fcac
Edr2 <>
2024-11-04 21:35:07 +0100

    docker works

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Test Controller Pod Dockerfile
+FROM python:3.9-slim
+
+# Install Chrome and Chrome Driver
+RUN apt-get update && apt-get install -y \
+    wget \
+    gnupg \
+    unzip \
+    && wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_130.0.6723.91-1_amd64.deb \
+    && apt install -y ./google-chrome-stable_130.0.6723.91-1_amd64.deb \
+    && wget -O /tmp/chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/130.0.6723.91/linux64/chrome-linux64.zip \
+    && unzip /tmp/chromedriver.zip -d /usr/local/bin/ \
+    && rm /tmp/chromedriver.zip
+
+# Install Python dependencies
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy test files
+COPY . /app
+WORKDIR /app
+
+# Command to run tests
+CMD ["python", "-m", "unittest", "tests/test_insider.py"]
--- tests/test_insider.py
+++ tests/test_insider.py
@@ -34,50 +34,52 @@ class InsiderTest(unittest.TestCase):
             self.driver.quit()
             logger.info("Closing WebDriver")
 
-    # def test_01_homepage(self):
-    #     """Test Insider homepage loads correctly"""
-    #     self.driver.get("https://useinsider.com/")
-    #     assert "Insider" in self.driver.title
-    #     logger.info("Homepage test passed")
+    def test_01_homepage(self):
+        """Test Insider homepage loads correctly"""
+        self.driver.get("https://useinsider.com/")
+        assert "Insider" in self.driver.title
+        logger.info("Homepage test passed")
 
-    # def test_02_careers_page(self):
-    #     """Test Careers page and its components"""
-    #     self.driver.get("https://useinsider.com/")
+    def test_02_careers_page(self):
+        """Test Careers page and its components"""
+        self.driver.get("https://useinsider.com/")
 
-    #     # Step 1: Check and close the popup if it exists
-    #     try:
-    #         # Locate the popup close button by its class name and click it
-    #         popup_close_button = self.wait.until(
-    #             EC.element_to_be_clickable(
-    #                 (By.CLASS_NAME, "ins-close-button")))
-    #         popup_close_button.click()
-    #     except TimeoutException:
-    #         pass  # No popup appeared, continue with the test
+        # Step 1: Check and close the popup if it exists
+        try:
+            # Locate the popup close button by its class name and click it
+            popup_close_button = self.wait.until(
+                EC.element_to_be_clickable(
+                    (By.CLASS_NAME, "ins-close-button")))
+            popup_close_button.click()
+        except TimeoutException:
+            pass  # No popup appeared, continue with the test
 
-    #     company_menu = self.wait.until(
-    #         EC.element_to_be_clickable((By.LINK_TEXT, "Company")))
-    #     company_menu.click()
+        company_menu = self.wait.until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "Company")))
+        company_menu.click()
 
-    #     careers_link = self.wait.until(
-    #         EC.element_to_be_clickable((By.LINK_TEXT, "Careers")))
-    #     careers_link.click()
+        careers_link = self.wait.until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "Careers")))
+        careers_link.click()
 
-    #     # Verify components
-    #     self.wait.until(
-    #         EC.presence_of_element_located((By.CLASS_NAME, "location-slider-prev")))
-    #     self.wait.until(EC.presence_of_element_located((By.LINK_TEXT, "See all teams")))
+        # Verify components
+        self.wait.until(
+            EC.presence_of_element_located(
+                (By.CLASS_NAME, "location-slider-prev")))
+        self.wait.until(
+            EC.presence_of_element_located((By.LINK_TEXT, "See all teams")))
 
-    #     h2_elements = self.driver.find_elements(By.TAG_NAME, "h2")
-    #     found = any("Life at Insider" in element.text for element in h2_elements)
-    #     self.assertTrue(found, "No <h2> elements with text 'Life at Insider' were found.")
+        h2_elements = self.driver.find_elements(By.TAG_NAME, "h2")
+        found = any("Life at Insider" in element.text
+                    for element in h2_elements)
+        self.assertTrue(
+            found, "No <h2> elements with text 'Life at Insider' were found.")
 
-    #     logger.info("Careers page test passed")
+        logger.info("Careers page test passed")
 
     def test_03_qa_jobs(self):
         # """Test QA jobs filtering and content"""
         # self.driver.get("https://useinsider.com/careers/quality-assurance/")
-
-        # print("TEST -- See all QA jobs start")
 
         # # Click See all QA jobs
         # see_all_jobs = self.wait.until(
@@ -85,24 +87,47 @@ class InsiderTest(unittest.TestCase):
         # )
         # see_all_jobs.click()
 
-        self.driver.get("https://useinsider.com/careers/open-positions")
+        #TODO It should go to that link, but after investigated and seeing that frontend send 4 same requests to fetch department, I give up for now
+        #self.driver.get("https://useinsider.com/careers/open-positions/
+
+        self.driver.get(
+            "https://useinsider.com/careers/open-positions/?department=qualityassurance"
+        )
 
         decline_cookie = self.wait.until(
             EC.element_to_be_clickable((By.LINK_TEXT, "Decline All")))
         decline_cookie.click()
 
-        time.sleep(5)
+        # TODO element should be disabled in UI(frontend) till it's loaded
+        # Maximum number of attempts to retry clicking on the dropdown
+        max_attempts = 3
+        attempt = 0
 
-        # Filter jobs
-        location_filter = self.wait.until(
-            EC.element_to_be_clickable(
-                (By.ID, "select2-filter-by-location-container")))
-        location_filter.click()
+        while attempt < max_attempts:
+            try:
+                # Attempt to click the location filter
+                location_filter = self.wait.until(
+                    EC.element_to_be_clickable(
+                        (By.ID, "select2-filter-by-location-container")))
+                location_filter.click()
 
-        istanbul_option = self.wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//li[contains(text(), 'Istanbul, Turkey')]")))
-        istanbul_option.click()
+                # Wait and try to find the Istanbul option
+                istanbul_option = self.wait.until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH,
+                         "//li[contains(text(), 'Istanbul, Turkey')]")))
+                istanbul_option.click()
+                logger.info("Successfully clicked on Istanbul option.")
+                break  # Exit the loop if successful
+
+            except TimeoutException:
+                attempt += 1
+                logger.info(f"Attempt {attempt} failed. Retrying...")
+                if attempt == max_attempts:
+                    logger.info(
+                        "Failed to find and click the Istanbul option after several attempts."
+                    )
+                    raise  # Re-raise the exception after maximum retries
 
         department_filter = self.wait.until(
             EC.element_to_be_clickable(
@@ -112,9 +137,7 @@ class InsiderTest(unittest.TestCase):
         # Wait for the <ul> element to be present
         ul_element = self.wait.until(
             EC.presence_of_element_located(
-                (By.ID, "select2-filter-by-department-results"
-                 ))
-        )
+                (By.ID, "select2-filter-by-department-results")))
 
         # Scroll down the <ul> element
         self.driver.execute_script(
@@ -123,27 +146,29 @@ class InsiderTest(unittest.TestCase):
         qa_option = self.wait.until(
             EC.element_to_be_clickable(
                 (By.XPATH, "//li[contains(text(), 'Quality Assurance')]")))
-        li_element = self.driver.find_element(
-            By.XPATH, "//li[contains(text(), 'Quality Assurance')]")
+
+        # TODO If there will be 1 request instead 6 with same response, maybe we can implement this code below
+        # li_element = self.driver.find_element(
+        #     By.XPATH, "//li[contains(text(), 'Quality Assurance')]")
         # self.driver.execute_script("arguments[0].scrollIntoView(true);",
-                                #    li_element)
+        #    li_element)
         # self.driver.execute_script("arguments[0].focus();", li_element)
         # self.driver.execute_script(
         #     "arguments[0].setAttribute('aria-selected', arguments[1]);",
         #     li_element, "true")
         # self.driver.execute_script("arguments[0].click();", li_element)
-
         # li_element.click()
-        # li_element.send_keys(Keys.ENTER)
 
-        #select2-results__options
-
-        # li_element.click()
         actions = ActionChains(self.driver)
-        actions.move_to_element(li_element).click().perform()
-    
+        actions.move_to_element(qa_option).click().perform()
 
+        #even when I explicitly set flag ?department=qualityassurance it show on start wrong departments :(
+        # wait_more = WebDriverWait(self.driver, 15)
         time.sleep(5)
+        self.wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//span[text()='Quality Assurance']")))
+
         # Verify job listings
         self.wait.until(
             EC.visibility_of_element_located(
@@ -152,19 +177,18 @@ class InsiderTest(unittest.TestCase):
             EC.presence_of_all_elements_located(
                 (By.CLASS_NAME, "position-list-item")))
 
-        print("TEST -- jobs loop start")
         for job in jobs:
             position = job.find_element(By.CLASS_NAME, "position-department")
             position_text = position.text
             location = job.find_element(By.CLASS_NAME,
                                         "position-location").text
 
-            print("TEST -- position_text: " + position_text)
+            logger.info("TEST -- position_text: " + position_text)
 
             assert "Quality Assurance" in position_text
             assert "Istanbul, Turkey" in location
 
-            print("TEST -- view_role_btn start")
+            logger.info("TEST -- view_role_btn start")
 
             self.driver.execute_script(
                 "arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});",
